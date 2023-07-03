fe2844483ecd0cb1ea84a89709f9dab0c533012d
odoo-app-dev <homayounfar@msn.com>
2023-07-03 11:02:39 +0330

    commit 1.0.7

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,3 @@ from . import base_info
 from . import input_info
 from . import contract_info
 from . import contract_registration
-# from . import anomaly
-# from . import anomaly
-# from . import project
-# from . import permit
-# from . import weather
-# from . import personnel
-# from . import incident
-# from . import drill
-# from . import training
-# from . import actions
