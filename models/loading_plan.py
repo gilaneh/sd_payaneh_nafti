26a7ffd201d22158bc82277e4717ec3332ea3b2e
arash <arash@Arash.arash>
2024-03-24 07:20:46 -0400

    dashbaord, open contracts of the day

--- models/loading_plan.py
+++ models/loading_plan.py
@@ -74,9 +74,9 @@ class SdPayanehNaftiLoadingPlan(models.Model):
         for day in range(3):
             the_day = start_day + timedelta(days=day)
             contracts = self.env['sd_payaneh_nafti.contract_registration'].sudo().search([ '|', '|',
-                                                                                          ('end_date', '>', the_day),
-                                                                                          ('first_extend_end_date', '>', the_day),
-                                                                                          ('second_extend_end_date', '>', the_day),
+                                                                                          ('end_date', '>=', the_day),
+                                                                                          ('first_extend_end_date', '>=', the_day),
+                                                                                          ('second_extend_end_date', '>=', the_day),
 
                                                                                           ])
             contracts = list([rec for rec in contracts if rec.remain_amount > 100])
@@ -99,8 +99,8 @@ class SdPayanehNaftiLoadingPlan(models.Model):
         lang = self.env.context.get('lang', 'en_US')
         start_day = datetime.now(pytz.timezone(self.env.context.get('tz', 'Asia/Tehran'))).date()
         end_day = datetime.now(pytz.timezone(self.env.context.get('tz', 'Asia/Tehran'))).date() + timedelta(days=3)
-        plans = self.sudo().search([('record_date', '>', start_day),
-                                    ('record_date', '<', end_day), ])
+        plans = self.sudo().search([('record_date', '>=', start_day),
+                                    ('record_date', '<=', end_day), ])
         # for plan in plans:
         # #     # print(f'>>>>> plans: {plan.registration_no.registration_no:^6}  {plan.record_date:^12} plan: {plan.load:>5}  load: {plan.load:>5}')
         #     _ = plan.load
